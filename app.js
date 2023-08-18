9301124c2deeec3b370175b1cc7c67c2dcafb7e4
ankitjeevijay <ankitjeevijay@gmail.com>
2023-08-18 17:23:04 +0530

    ok

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 const dotenv = require('dotenv')
 const connectdb = require('./db/connectdb')
 const web = require('./routes/web')
+const cors = require('cors')
+app.use(cors())
 
 app.use(express.json())
 
